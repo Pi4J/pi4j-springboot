564820e26c14b32b7a2a5bde9b53cc4d2a86de6d
DaShaun <dashaun@dashaun.com>
2023-01-30 18:46:07 -0600

    from javagrunt stream

--- pi4j-autoconfigure/src/main/java/com/pi4j/autoconfigure/ContextAutoConfiguration.java
+++ pi4j-autoconfigure/src/main/java/com/pi4j/autoconfigure/ContextAutoConfiguration.java
@@ -1,8 +1,6 @@
 package com.pi4j.autoconfigure;
 
-import com.pi4j.context.Context;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Import;
 
 @AutoConfiguration

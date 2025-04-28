90a8d7a378622d27c6c888a3bdf4efb337b90d50
Frank Delporte <frank@webtechie.be>
2025-04-28 08:13:37 +0200

    Bump to Pi4J 3.0.1

--- pi4j-spring-boot-starter-sample-app/src/test/java/com/pi4j/spring/boot/sample/app/ContextConfigurationTests.java
+++ pi4j-spring-boot-starter-sample-app/src/test/java/com/pi4j/spring/boot/sample/app/ContextConfigurationTests.java
@@ -3,9 +3,11 @@ package com.pi4j.spring.boot.sample.app;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.Configuration;
 
 import static org.springframework.boot.autoconfigure.AutoConfigurations.of;
 
+@Configuration
 public class ContextConfigurationTests {
 
     private final ApplicationContextRunner contextRunner =
